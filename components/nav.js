100f37dabebcd0cc709117814555e19badfc67d3
tabletopjosh <joshuaarouni@gmail.com>
2025-01-17 17:31:52 -0500

    another try

--- components/nav.js
+++ components/nav.js
@@ -8,7 +8,7 @@ import Link from 'next/link';
 function Nav() {
     useEffect(() => {
         const hamburger = document.querySelector('.hamburger');
-        //hieee
+        //button test!
         const navContent = document.querySelector('.nav-content');
     
         hamburger.addEventListener('click', () => {
