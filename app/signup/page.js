a01ead37798f304ae715fd4eb1f5a7d8f905c9c9
lastusername <justindm1914@gmail.com>
2025-01-21 10:22:34 -0500

    Created login.css and made changes to page.js and nav.css

---
+++ app/signup/page.js
@@ -0,0 +1,15 @@
+import ".login.css";
+import leftArrow from "../../icons/Left_Arrow.png";
+function signIn() {
+    return (
+<>
+<nav>
+    <img className="return" src={leftArrow}>
+
+    </img>
+</nav>
+</>
+    )
+}
+
+export default signIn;
