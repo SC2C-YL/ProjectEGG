fe15ca54eaacf802ce871db704f74765cf069cb0
lastusername <justindm1914@gmail.com>
2025-02-03 23:41:55 -0500

    created filters buttons component

--- app/scholarships/page.jsx
+++ app/scholarships/page.jsx
@@ -1,11 +1,20 @@
 import Nav from "../../components/nav";
 import './scholarship.css';
+import FButton from "../../components/filter-grid-button";
 
 function scholarships() {
     return(
         <div>
             <Nav />
-            <h1>scholarships</h1>
+            <div className="H1-Div">
+                <h1>scholarships</h1>
+            </div>
+            <div className="filter-gird">
+                <FButton filter="Location"/>
+                <FButton filter="GPA"/>
+                <FButton filter="Fields of Study"/>
+            </div>
+
         </div>
     )
 }
---
+++ components/filter-grid-button.js
@@ -0,0 +1,15 @@
+
+
+function FButton({filter}) {
+
+
+    return (
+        <>
+        <button className="FButton">
+            {filter}
+        </button>
+        </>
+    )
+}
+
+export default FButton;
