73e097e59ca75d0701a427a2c378ed5718b85e7c
lastusername <justindm1914@gmail.com>
2025-02-04 21:06:49 -0500

    Created Scholarships page and componennt

--- app/scholarships/page.jsx
+++ app/scholarships/page.jsx
@@ -1,20 +1,31 @@
 import Nav from "../../components/nav";
 import './scholarship.css';
 import FButton from "../../components/filter-grid-button";
-
+import ScholarshipPreview from "../../components/scholarship-preview";
 function scholarships() {
     return(
         <div>
             <Nav />
             <div className="H1-Div">
-                <h1>scholarships</h1>
+                <h1>Scholarships</h1>
             </div>
             <div className="filter-gird">
                 <FButton filter="Location"/>
                 <FButton filter="GPA"/>
-                <FButton filter="Fields of Study"/>
+                <FButton filter="No Essay"/>
+                <FButton filter="Essay"/>
             </div>
-
+            <div className="scholars-boxes">
+            <ScholarshipPreview possibleDeadline = "Deadline Soon" Info="Blah Blah Blah Scholarship; Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah" Title="Scholarship" Odate="##/##/###" Cdate="##/##/###"/>
+            <ScholarshipPreview possibleDeadline = "Deadline Soon" Info="Information" Title="Scholarship" Odate="##/##/###" Cdate="##/##/###"/>
+            <ScholarshipPreview possibleDeadline = "Deadline Soon" Info="Information" Title="Scholarship" Odate="##/##/###" Cdate="##/##/###"/>
+            <ScholarshipPreview possibleDeadline = "Deadline Soon" Info="Information" Title="Scholarship" Odate="##/##/###" Cdate="##/##/###"/>
+            <ScholarshipPreview possibleDeadline = "Deadline Soon" Info="Information" Title="Scholarship" Odate="##/##/###" Cdate="##/##/###"/>
+            <ScholarshipPreview possibleDeadline = "Deadline Soon" Info="Information" Title="Scholarship" Odate="##/##/###" Cdate="##/##/###"/>
+            <ScholarshipPreview possibleDeadline = "Deadline Soon" Info="Blah Blah Blah Scholarship; Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah" Title="Scholarship" Odate="##/##/###" Cdate="##/##/###"/>
+            <ScholarshipPreview possibleDeadline = "Deadline Soon" Info="Information" Title="Scholarship" Odate="##/##/###" Cdate="##/##/###"/>
+            
+        </div>
         </div>
     )
 }
---
+++ components/scholarship-preview.js
@@ -0,0 +1,32 @@
+function ScholarshipPreview({ possibleDeadline, Title, Info, Odate, Cdate}) {
+
+    return (
+            <div className="scholar-box">
+                <div className="deadline-box">
+                    <h3 className="deadline-text">{possibleDeadline}</h3>
+                </div>
+                <div className="title-box">
+                    <h2 className="scholar-title">{Title}</h2>
+                </div>
+                <div className="Scholar-info-box">
+                    <p className="Scholar-info-text">{Info}</p>
+                    <div>
+                        <hr className="Line"></hr>
+                        <div className="Sdates">
+                            <h3 className="Bolder">Open Date</h3>
+                            <h4 className="Bolder">{Odate}</h4>
+                        </div>
+                        <div className="Sdates">
+                            <h3 className="Bolder">Close Date</h3>
+                            <h4 className="Bolder">{Cdate}</h4>
+                        </div>
+                        <div className="Apply-box">
+                            <button className="Apply">Apply now</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    )
+}
+
+export default ScholarshipPreview;
