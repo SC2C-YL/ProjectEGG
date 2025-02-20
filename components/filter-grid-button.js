f701c61c53c4f48dd3517b397f59ba4cbe423a33
Justin <justin@Justins-MacBook-Pro.local>
2025-02-20 00:45:17 -0500

    making navbar more legible

--- components/filter-grid-button.js
+++ components/filter-grid-button.js
@@ -1,11 +1,31 @@
+"use client"
 
-
+import {useState} from "react";
 function FButton({filter}) {
+    const [isActive, setIsActive] = useState(true);
 
+    const fbutton = {
+        marginLeft: "1rem",
+        marginRight: "1rem",
+       backgroundColor: isActive ? " #CDCDCD" : "#FFD966",
+       borderRadius: "25px",
+       padding: "1%",
+       paddingLeft: "2%",
+       paddingRight: "2%",
+       fontWeight: "1000",
+       fontSize: "1.5rem",
+       color: isActive ? "#4C4646": "#AF8400",
+       marginBottom: "1rem",
+    }
+
+    const toggleStyling = () => {
+        setIsActive(prevState => !prevState)
+
+    }
 
     return (
         <>
-        <button className="FButton">
+        <button style={fbutton} onClick={toggleStyling}>
             {filter}
         </button>
         </>
